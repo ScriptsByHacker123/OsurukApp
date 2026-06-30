285dcb0cf9da6a2cd4355d360747aad958fa494a
Scripts1234143 <alverahmetenes@gmail.com>
2026-06-30 22:36:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 import webbrowser
 
 class MuzikUygulamasi(BoxLayout):
